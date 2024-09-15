745e69c9ee66be5d752d6591c49a5f7b1f5efd15
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-09-14 22:03:28 -0300

    implementada a lógica da classe pedido nacional

---
+++ PedidoNacional.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaGestaoProdutos
+{
+    public class PedidoNacional : Pedido
+    {
+        public double Imposto { get; set; }
+
+        public PedidoNacional(int id_pedido, string nome_Produto, int quantidade, double preco_Produto, string data_Pedido, double imposto) : base(id_pedido, nome_Produto, quantidade, preco_Produto, data_Pedido)
+        {
+            Imposto = imposto;
+        }
+        public double TaxaImpostoNacional() => Preco_Produto * Imposto+ Quantidade * Preco_Produto;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,11 +6,16 @@ namespace SistemaGestaoProdutos
     {
         public static void Main(string[] args)
         {
-            Pedido pedido = new Pedido(1, "Guitarra", 3, 7500, "13/09/2024");
+            Pedido pedido = new Pedido(1, "Guitarra", 3, 7000, "13/09/2024");
 
             pedido.CalculandoTotal();
 
             Console.WriteLine(pedido.CalculandoTotal());
+
+            PedidoNacional pedidoNacional = new PedidoNacional(2, "contra-baixo", 1, 100, "14/09/2024", 0.1);
+            pedidoNacional.TaxaImpostoNacional();
+            Console.WriteLine(pedidoNacional.TaxaImpostoNacional());
+            ;
         }
     }
 }
